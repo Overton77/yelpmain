e73518b063dff777475200ee3ae1f709ce1ce1b4
Overton77 <133080874+Overton77@users.noreply.github.com>
2024-01-09 16:56:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,  
-  {dbName: 'yelp-camp'}
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
