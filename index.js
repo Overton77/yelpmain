323cd52c3a5b6f1dca315abe5ba767f76afc0342
Overton77 <133080874+Overton77@users.noreply.github.com>
2024-01-09 17:07:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ const reviewRoutes = require("./routes/reviews.js");
 
 const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
